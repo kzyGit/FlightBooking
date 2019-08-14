7f46fe9336969086cec9e8cb85d2d52a6cb251c8
Kezzy Angiro <kezzyangiro@Kezzys-MacBook-Pro.local>
2019-08-14 20:01:03 +0300

    initial commit

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views.user import HomeView, UserView, UserLoginView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home")
+    path('', HomeView.as_view(), name="home"),
+    path('users/', UserView.as_view(), name='signup'),
+    path('login/', UserLoginView.as_view(), name='login')
 ]
